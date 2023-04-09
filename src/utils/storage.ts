8b71bc4602c84feab560556df4a3379d981c20b9
opcode3 <opcode3@gmail.com>
2023-04-09 08:02:33 -0700

    transfering files

---
+++ src/utils/storage.ts
@@ -0,0 +1,24 @@
+import { hasCookie, setCookies, getCookie, deleteCookie } from "cookies-next";
+import type { CookieValueTypes } from "cookies-next";
+
+const WEEK_IN_SECONDS = 604800;
+
+export default class Storage {
+  set(key: string, value: CookieValueTypes) {
+    return setCookies(key, value, {
+      maxAge: WEEK_IN_SECONDS,
+    });
+  }
+
+  get(key: string) {
+    return getCookie(key);
+  }
+
+  check(key: string) {
+    return hasCookie(key);
+  }
+
+  remove(key: string) {
+    deleteCookie(key);
+  }
+}
