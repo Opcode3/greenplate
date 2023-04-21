50396b0e7cdffcc18f1f73f47aad0723d9f715f4
Joseph Emmanuel Emeka <opcode3@gmail.com>
2023-04-21 08:48:40 +0100

    auth class added

---
+++ src/services/auths.tsx
@@ -0,0 +1,27 @@
+type RestaurantDataType = {
+
+}
+
+
+export default class Auths{
+    constructor(){
+
+    }
+
+    login(data: {email: string, password: string}){
+
+    }
+
+    register(data: {email: string, password: string}){
+
+    }
+
+
+    logInRestaurant(data: RestaurantDataType){
+
+    }
+
+    registerRestaurant(data: RestaurantDataType){
+
+    }
+}
